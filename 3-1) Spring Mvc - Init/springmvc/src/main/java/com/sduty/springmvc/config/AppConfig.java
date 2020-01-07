a09e6196325a5cfcda1775c3924dac6a4866da3c
nankisu0301 <nankisu0301@naver.com>
2020-01-08 00:57:55 +0900

    3-1) Spring Mvc - Init, Validation

--- 3-1) Spring Mvc - Init/springmvc/src/main/java/com/sduty/springmvc/config/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package com.sduty.springmvc.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfig{
-
-}
--- 3-1) Spring Mvc - Init/springmvc/src/main/webapp/WEB-INF/index.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-	<h2>Hello World!</h2>
-	<ul>
-		<li><a href="<c:url value='/echo'/>">에코로 이동</a></li>
-	</ul>
-</body>
-</html>
