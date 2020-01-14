5346b7778f44abcccb65c0717843f23fe79866a0
nankisu0301 <nankisu0301@naver.com>
2020-01-14 22:51:26 +0900

    4-1) Spring Web App : handler method parameters

---
+++ 4-1) Spring Web App/webapp/src/main/java/com/study/webapp/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.study.webapp.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
---
+++ 4-1) Spring Web App/webapp/src/main/java/com/study/webapp/config/DispatcherServletConfig.java
@@ -0,0 +1,21 @@
+package com.study.webapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.study.webapp.controller"})
+public class DispatcherServletConfig extends WebMvcConfigurerAdapter {
+  @Bean
+  public InternalResourceViewResolver internalResourceViewResolver() {
+    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+    resolver.setPrefix("/views/");
+    resolver.setSuffix(".jsp");
+    return resolver;
+  }
+}
