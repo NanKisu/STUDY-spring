b24fd28d7a1cca3e64ae1731e8856569d0e7fe80
nankisu0301 <nankisu0301@naver.com>
2020-02-07 23:56:16 +0900

    5-1) Spring Web App - PageMove

---
+++ 5-1) Spring Web App - PageMove/pagemove/src/main/java/com/study/webapp/pagemove/config/ServletContextConfig.java
@@ -0,0 +1,16 @@
+package com.study.webapp.pagemove.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = {"com.study.webapp.pagemove.controller"})
+public class ServletContextConfig extends WebMvcConfigurerAdapter{
+  @Bean
+  public InternalResourceViewResolver internalResourceViewResolver() {
+    return new InternalResourceViewResolver("/", ".jsp");
+  }
+}
