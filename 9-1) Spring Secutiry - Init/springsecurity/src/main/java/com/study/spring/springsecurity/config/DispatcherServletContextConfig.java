950ad5c7e4e35e6289e61c8a7034b2efb0d4d8fa
nankisu0301 <nankisu0301@naver.com>
2020-04-08 00:00:44 +0900

    9-1) Spring Secutiry - Init

--- 9-1) Spring Secutiry - Init/springsecurity/src/main/java/com/study/spring/springsecurity/config/DispatcherServletContextConfig.java
+++ 9-1) Spring Secutiry - Init/springsecurity/src/main/java/com/study/spring/springsecurity/config/DispatcherServletContextConfig.java
@@ -1,13 +1,10 @@
 package com.study.spring.springsecurity.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-
-@ComponentScan(basePackages = {"com.study.spring.springsecurity"})
-@Import(value = {WebMvcConfig.class})
+@Import(value = {SecurityConfig.class})
 public class DispatcherServletContextConfig{
 
 }
