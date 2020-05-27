1117f4e02ed4822bc3b5ded632685af2b8539460
nankisu0301 <nankisu0301@naver.com>
2020-05-27 22:18:40 +0900

    10-1) Spring JAP - Init, model, repository, CRUD, paging

--- 10-1) Spring JAP - Init, model, repository/springjap/src/main/java/com/study/spring/springjpa/config/DBConfig.java
+++ 10-1) Spring JAP - Init, model, repository/springjap/src/main/java/com/study/spring/springjpa/config/DBConfig.java
@@ -1,6 +1,8 @@
 package com.study.spring.springjpa.config;
 
+import java.sql.SQLException;
 import javax.sql.DataSource;
+import org.mariadb.jdbc.MariaDbDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
--- 10-1) Spring JAP - Init, model, repository/springjap/src/main/java/com/study/spring/springjpa/controller/HomeController.java
+++ 10-1) Spring JAP - Init, model, repository/springjap/src/main/java/com/study/spring/springjpa/controller/HomeController.java
@@ -4,19 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.study.spring.springjpa.model.MyUser;
 import com.study.spring.springjpa.repository.MyUserRepository;
+import com.study.spring.springjpa.service.MyUserService;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
   @Autowired
-  private MyUserRepository myUserRepository;
+  private MyUserService myUserService;
   
   @GetMapping
   public String home() {
     System.out.println("Welcome!");
-    System.out.println(myUserRepository);
-    System.out.println(myUserRepository.findAll());
+    MyUser myUser = new MyUser();
+    myUser.setAge(20);
+    myUser.setName("kisued");
+    myUserService.createMyUser(myUser);
+    System.out.println(myUserService.findByAge(20));
+    System.out.println(myUserService.findByName("kisued"));
     return "home";
   }
   
